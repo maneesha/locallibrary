20e9f21e448f20a22ac79d0b2cd12f668e78d30b
maneesha <ahseenam@gmail.com>
2022-10-01 14:48:30 -0400

    1 - create generic views for BookList and BookDetail

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic 
 
 # Create your views here.
 
@@ -27,4 +28,10 @@ def index(request):
 
     # Render the HTML template index.html with data from context
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
 
