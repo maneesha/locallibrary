b048144a70f1590a8a9cec46fb3dcac0819ec181
maneesha <ahseenam@gmail.com>
2022-10-01 13:01:36 -0400

    add urlpattern for index page

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,4 +2,15 @@ import imp
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+
+    # Three params for path: 
+    # URL pattern is empty string, 
+    # view function - function named index in views.py
+    # name is used to reverse mapper so you can do this in a template:
+    # <a href="{% url 'index' %}">Home</a>
+    # instead of 
+    # <a href="/catalog/">Home</a> 
+    # which ill break if we change pattern to home page
+    path('', views.index, name='index')
+]
