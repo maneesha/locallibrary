7e82cc421eab43cd0737181d422be8ccddf91d40
maneesha <ahseenam@gmail.com>
2022-10-01 09:50:15 -0400

    set up Genre model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db import reverse # used to generate URLs by reversing the URL patterns 
 
 # Create your models here.
+
+class Genre(models.Model):
+    """Model representing a book genre"""
+    name = models.CharField(max_length=200, help_text="Enter a book genre")
+
+    def __str__(self):
+        """String for representing the model object"""
+        return self.name
+
+
