faaaea5a3d22fc1f53a3a447b414ff0858d1ee22
maneesha <ahseenam@gmail.com>
2022-10-01 16:18:57 -0400

    fix DEBUG and SECRET KEY settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
