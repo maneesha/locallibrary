43bb4334c7121903f10b9afa6d5b677496f94303
maneesha <ahseenam@gmail.com>
2022-10-01 11:14:00 -0400

    update author admin list view

--- catalog/admin.py
+++ catalog/admin.py
@@ -12,7 +12,7 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    pass 
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death') 
 admin.site.register(Author, AuthorAdmin)
 
 
