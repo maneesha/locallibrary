af983b5783ab515ed20f561aaa668689273e9a04
maneesha <ahseenam@gmail.com>
2022-10-01 11:11:10 -0400

    set up all the admin classes

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,34 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(BookInstance)
-admin.site.register(Language)
+# admin.site.register(Book)
+# Instead of simple admin.site.register we can crate class to register AuthorAdmin. This allows customization of admin panel.
+# admin.site.register(Author)
+# admin.site.register(Genre)
+# admin.site.register(BookInstance)
+# admin.site.register(Language)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass 
+admin.site.register(Author, AuthorAdmin)
+
+
+@admin.register(Book)  # This decorator does exactly the same thing as the admin.site.register() syntax
+class BookAdmin(admin.ModelAdmin):
+    pass 
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass 
